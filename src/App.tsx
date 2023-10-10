0be234e6f97999855cbf672ab400c0a23acfc526
Trond Albinussen <trond.albinussen@blinq.no>
2023-10-10 12:03:11 +0200

    material, font and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 // import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
+import Typography from '@mui/material/Typography';
 import './App.css';
 
 function App() {
-  return <div>Hello there!!</div>;
+  return <Typography>Hello there!!</Typography>;
 }
 
 export { App };
